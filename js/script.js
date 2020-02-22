e29608d831d278313426d6646ff141030e8d775f
MrGrease <mehmetemreyorulmaz@gmail.com>
2020-02-23 00:36:46 +0300

    Added get function from the api with ajax and CORS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
 $(function() {
+  var apiurl =
+    'http://cors-anywhere.herokuapp.com/https://www.bloomberght.com/piyasa/intradaydata/dolar';
 
+  $.ajax({
+    url: apiurl,
+    contentType: 'application/json',
+    dataType: 'json',
+    responseType: 'application/json',
+    xhrFields: {
+      withCredentials: false
+    },
+    headers: {
+      'Access-Control-Allow-Credentials': true,
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET',
+      'Access-Control-Allow-Headers': 'application/json'
+    },
+    success: function(data) {
+      console.log(data);
+    }
+  });
 });
