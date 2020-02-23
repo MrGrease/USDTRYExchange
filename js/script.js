6b5ae2e0979fecac50a0dcdd0142e4d038f2f3f6
MrGrease <mehmetemreyorulmaz@gmail.com>
2020-02-23 03:01:59 +0300

    Added HTML table and button, bootstrap,modified CSS, implemented data modification function ,implemented button refresh function, used JQUERY to display data on table

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
-$(function() {
-  var apiurl =
-    'http://cors-anywhere.herokuapp.com/https://www.bloomberght.com/piyasa/intradaydata/dolar';
+var last = function last(array, n) {
+  if (array == null) return void 0;
+  if (n == null) return array[array.length - 1];
+  return array.slice(Math.max(array.length - n, 0));
+};
 
+var refreshall = function refreshall(lastten) {
+  $('.Time').each(function(index) {
+    var d = new Date(lastten[index][0]);
+    $(this).html(d);
+  });
+  $('.rate').each(function(index) {
+    $(this).html(lastten[index][1]);
+  });
+  console.log('refreshing');
+};
+var lastten;
+
+var apiurl =
+  'http://cors-anywhere.herokuapp.com/https://www.bloomberght.com/piyasa/intradaydata/dolar';
+
+function getData() {
   $.ajax({
     url: apiurl,
     contentType: 'application/json',
@@ -17,7 +35,17 @@ $(function() {
       'Access-Control-Allow-Headers': 'application/json'
     },
     success: function(data) {
-      console.log(data);
+      var series = data.SeriesData;
+      lastten = last(series, 10);
+      refreshall(lastten);
     }
   });
+}
+
+$('#refresh').click(function() {
+  getData(lastten);
+});
+
+$(function() {
+  getData();
 });
